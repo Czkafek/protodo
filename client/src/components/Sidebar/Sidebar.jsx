593781cdd7c858dbdcf85e94e5b758f8ec526a74
Czkafek <czkafek@gmail.com>
2025-02-07 13:54:17 +0100

    sidebar is done (visuals)

--- client/src/components/Sidebar/Sidebar.jsx
+++ client/src/components/Sidebar/Sidebar.jsx
@@ -1,13 +1,13 @@
 import styles from './Sidebar.module.css'
 import burgerMenuImg from '../../assets/sidebar/burger-menu.svg'
 import aiIcon from '../../assets/sidebar/ai.svg'
+import settingsIcon from '../../assets/sidebar/settings.svg'
 import foldersData from './folders'
 
 import { useEffect, useState } from 'react'
 
-function Sidebar() {
+function Sidebar({ isActive, setIsActive}) {
 
-    const [isActive, setIsActive] = useState(false);
     const [folders, setFolders] = useState(foldersData);
 
     const handleAddFolder = () => {
@@ -15,21 +15,25 @@ function Sidebar() {
     };
     
     return <div className={`${styles.sidebar} ${isActive ? styles.active : ''}`}>
-        <img src={burgerMenuImg} className={styles.burgerMenu} onClick={() => setIsActive(i => i = !i)} alt="menu icon" />
+        <img src={burgerMenuImg} className={styles.burgerMenu} onClick={() => setIsActive(!isActive)} alt="menu icon" />
         {   isActive &&
         <>
-        <div className={styles.askAIContainer}>
-            <img src={aiIcon} alt="AI icon" />
-            <p>Ask AI</p>
-        </div>
-        <div className={styles.foldersContainer}>
-            {
-                folders.map((folder, index) => {
-                    return <p>{folder}</p>
-                })
-            }
-            <p className={styles.addFolder} onClick={handleAddFolder} >Add +</p>
-        </div>
+            <div className={styles.iconOptionContainer}>
+                <img src={aiIcon} alt="AI icon" />
+                <p>Ask AI</p>
+            </div>
+            <div className={styles.foldersContainer}>
+                {
+                    folders.map((folder, index) => {
+                        return <p>{folder}</p>
+                    })
+                }
+                <p className={styles.addFolder} onClick={handleAddFolder} >Add +</p>
+            </div>
+            <div className={styles.iconOptionContainer} id={styles.settings}>
+                <img src={settingsIcon} />
+                <p>Settings</p>
+            </div>
         </>
         }
     </div>
