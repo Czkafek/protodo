0fbcb38c72722644a152e06b843ab7ec64fab833
Czkafek <czkafek@gmail.com>
2025-04-19 21:39:48 +0200

    symbolic

--- server/server.js
+++ server/server.js
@@ -7,10 +7,7 @@ const port = 8080;
 
 
 // MongoDB connection
-mongoose.connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
+mongoose.connect(process.env.MONGODB_URI)
 .then(() => console.log('MongoDB connected'))
 .catch(err => console.log(err));
 
@@ -26,3 +23,12 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
+
+app.get('/login', (req, res) => {
+    try {
+        // Walidacja -> sprawdza w bazie, czy jest użytkownik o takim loginie -> jak jest to sprawdza czy hasła się zgadzają -> jak się zgadzają to tworzy refresh token i tworzy ciasteczko z nim oraz access token i wysyła go w odpowiedzi
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({ error: err });
+    }
+})
